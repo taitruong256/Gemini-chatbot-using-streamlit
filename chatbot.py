3d0bffae39488666bb7ab55b0724b2df78ab580c
Tai Truong Duc <taitruong256@gmail.com>
2025-08-13 18:02:43 +0700

    add: format chat massenge

--- chatbot.py
+++ chatbot.py
@@ -8,8 +8,14 @@ load_dotenv()
 GOOGLE_API_KEY=os.environ.get('GOOGLE_API_KEY')
 genai.configure(api_key=GOOGLE_API_KEY)
 
-user_input = st.text_input("Nhập nội dung:")
-
 model = genai.GenerativeModel('gemini-2.0-flash')
-response = model.generate_content(user_input)
-st.write(response.text)
+
+if prompt := st.chat_input('Nhập câu hỏi: '):
+    with st.chat_message('user'):
+        st.markdown(prompt)
+    
+    response = model.generate_content(prompt)
+    reply = response.text
+    
+    with st.chat_message('asistant'):
+        st.markdown(reply)
