bfaa219ed6e6eab2416c5968fc5ce27ac47e9746
Tai Truong Duc <taitruong256@gmail.com>
2025-08-14 09:49:52 +0700

    add: chat history

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # Gemini-chatbot-using-streamlit
+
+### 1. Create a virtual environment:
+```bash 
+python3 -m venv my_env
+.\my_env\Scripts\activate 
+```
+
+### 2. Install dependencies:
+```bash
+pip install -r requirements.txt
+```
+
+### 3. Run the chatbot:
+```bash
+streamlit run chatbot.py
+```
+
+### 4. Access the application in your browser at http://localhost:8501.
+
+### 5. Start chatting with the assistant!
--- chatbot.py
+++ chatbot.py
@@ -10,12 +10,27 @@ genai.configure(api_key=GOOGLE_API_KEY)
 
 model = genai.GenerativeModel('gemini-2.0-flash')
 
+if 'messages' not in st.session_state:
+    st.session_state.messages = [] 
+    
+for msg in st.session_state.messages:
+    with st.chat_message("user" if msg["role"] == "user" else "assistant"):
+        st.markdown(msg["content"])
+
 if prompt := st.chat_input('Nhập câu hỏi: '):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    
     with st.chat_message('user'):
         st.markdown(prompt)
+        
+    history_chat = ''
+    for msg in st.session_state.messages:
+        history_chat += f'{msg['role']}: {msg['content']} \n'
     
-    response = model.generate_content(prompt)
+    response = model.generate_content(history_chat)
     reply = response.text
+    
+    st.session_state.messages.append({"role": "assistant", "content": reply})
     
     with st.chat_message('asistant'):
         st.markdown(reply)
